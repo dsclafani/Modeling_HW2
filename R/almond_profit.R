c78a63afc3dfb0cd3c0d8791be9ff596224512ce
wyliehampson <whampson@bren.ucsb.edu>
2022-04-19 20:42:07 -0700

    Changed yield to profit. Created box plot.

--- R/almond_profit.R
+++ R/almond_profit.R
@@ -1,24 +1,44 @@
 #' Almond Yield Function
 #' 
 #' @param data The climate data frame including daily precipitation and minimum daily temperature data
+#' @param coef_1
+#' @param coef_2
+#' @param coef_3
+#' @param coef_4
 #' @return df Returns a dataframe containing each given year and it's corresponding almond yield anomaly
+#' 
 #' 
 # Function Definition
 
-#according to Lobell paper, minimum temperature data is to be taken from month 2 of each year
+#according to Lobell paper, minimum temperature data is to be taken from month 2 of each year and total precipitation for January
 
-library(here)
+almond_profit <- function(data, 
+                          price,
+                          coef_1 = -0.015,
+                          coef_2 = 0.0046,
+                          coef_3 = 0.07,
+                          coef_4 = 0.0043,
+                          const = 0.28) {
 
-#source(here("R", "almond_yield.R"))
+  clim_data <- data %>%
+    group_by(year, month) %>%
+    summarise(tmin = mean(tmin_c), precip = sum(precip))
 
-almond_profit <- function(yield, year, price) {
-  
-  yield <- (coef_1 * temp) - (coef_2 * tmin_feb ^ 2) - (coef_3 * precip_jan) + (coef_4 * precip_jan ^ 2) + const
-  
-  profit <- yield * price
-  
-  year_profit <- data.frame(year = year, yield = yield, profit = profit)
-  
-  return(year_profit)
-  
+  tmin_feb <- clim_data %>% filter(month == 2) %>% select(tmin)
+
+  precip_jan <- clim_data %>% filter(month == 1) %>% select(precip)
+
+  df <- left_join(tmin_feb, precip_jan) %>%
+    rename(tmin_feb = tmin, precip_jan = precip)
+
+  df <- df %>%
+    mutate(yield = (coef_1 * tmin_feb) - (coef_2 * tmin_feb ^ 2) - (coef_3 * precip_jan) + (coef_4 * precip_jan ^ 2) + const) %>% 
+    mutate(profit = price * yield) %>% 
+    select(year, year, profit)
+
+  return(df)
+
 }
+
+
+
